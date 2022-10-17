659fe7b49dee7785919a4606cc8ae77cea944bf0
anjaemin <t01089416422@gmail.com>
2022-10-17 14:54:54 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Outlet, Route, Routes } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { MainPage } from './pages';
 
-export default App;
+export const App: React.FC = () => (
+  <Routes>
+    <Route
+      path=""
+      element={
+        <>
+          <Outlet />
+        </>
+      }
+    >
+      <Route index element={<MainPage />} />
+    </Route>
+  </Routes>
+);
